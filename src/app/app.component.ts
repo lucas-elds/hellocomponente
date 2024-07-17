ca869b2c3f2b8bd192ba31d046e4f0232c208c38
Lucas <lucasemiliano619@gmail.com>
2024-07-16 22:49:27 -0300

    importação zone.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import 'zone.js';
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'hellocomponente';
